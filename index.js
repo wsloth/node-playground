b8a27afb566e9aa5c87b3c11f75ba34ea0955f14
Wessel Loth <wessel.loth@gmail.com>
2015-07-15 17:54:38 +0200

    Added database stuff

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
 
+// Database code
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -16,5 +32,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
